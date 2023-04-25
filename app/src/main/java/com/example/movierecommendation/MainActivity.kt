25ca0967b9e32b5ee8b2b56b2e38550a93c17c8e
LordLizard <WarriorXKz@gmail.com>
2023-04-25 14:58:52 +0300

    change design, add button

--- app/src/main/java/com/example/movierecommendation/MainActivity.kt
+++ app/src/main/java/com/example/movierecommendation/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.movierecommendation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movierecommendation.adapter.ItemAdapter
 import com.example.movierecommendation.data.Datasource
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +16,8 @@ class MainActivity : AppCompatActivity() {
         val myDataset = Datasource().loadMovieCards()
         val recycleView = findViewById<RecyclerView>(R.id.recycleView)
         recycleView.adapter = ItemAdapter(this, myDataset)
+    }
+    fun onMyButtonClick(view: View?) {
+        Toast.makeText(this, "Зачем вы нажали?", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/movierecommendation/data/Datasource.kt
+++ app/src/main/java/com/example/movierecommendation/data/Datasource.kt
@@ -7,15 +7,14 @@ class Datasource {
     fun loadMovieCards() :List<MovieCard>{
         return listOf<MovieCard>(
             MovieCard(R.string.movie_brat, R.drawable.movie_brat, R.string.movie_brat_description),
-            MovieCard(R.string.movie_brat, R.drawable.movie_brat, R.string.movie_brat_description),
-            MovieCard(R.string.movie_brat, R.drawable.movie_brat, R.string.movie_brat_description),
-            MovieCard(R.string.movie_brat, R.drawable.movie_brat, R.string.movie_brat_description),
-            MovieCard(R.string.movie_brat, R.drawable.movie_brat, R.string.movie_brat_description),
-            MovieCard(R.string.movie_brat, R.drawable.movie_brat, R.string.movie_brat_description),
-            MovieCard(R.string.movie_brat, R.drawable.movie_brat, R.string.movie_brat_description),
-            MovieCard(R.string.movie_brat, R.drawable.movie_brat, R.string.movie_brat_description),
-            MovieCard(R.string.movie_brat, R.drawable.movie_brat, R.string.movie_brat_description),
-            MovieCard(R.string.movie_brat, R.drawable.movie_brat, R.string.movie_brat_description)
+            MovieCard(R.string.movie_brat2, R.drawable.movie_brat, R.string.movie_brat_description),
+            MovieCard(R.string.movie_brat3, R.drawable.movie_brat, R.string.movie_brat_description),
+            MovieCard(R.string.movie_brat4, R.drawable.movie_brat, R.string.movie_brat_description),
+            MovieCard(R.string.movie_brat5, R.drawable.movie_brat, R.string.movie_brat_description),
+            MovieCard(R.string.movie_brat6, R.drawable.movie_brat, R.string.movie_brat_description),
+            MovieCard(R.string.movie_brat7, R.drawable.movie_brat, R.string.movie_brat_description),
+            MovieCard(R.string.movie_brat8, R.drawable.movie_brat, R.string.movie_brat_description),
+            MovieCard(R.string.movie_brat9, R.drawable.movie_brat, R.string.movie_brat_description),
         )
     }
 }
